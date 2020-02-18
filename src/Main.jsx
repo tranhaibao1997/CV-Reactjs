16a741344f242865e452380108800bc5ca369b04
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-02-18 13:00:32 +0700

    fix loader

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,4 +1,4 @@
-import React,{useEffect} from 'react'
+import React,{useEffect, useState} from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
@@ -7,17 +7,22 @@ import Projects from './components/Projects/Projects.jsx'
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Loading from './components/Loading/Loading.jsx'
 
-
+const HomePage = React.lazy(() => import('./components/Home/Home'))
+const AboutPage = React.lazy(() => import('./components/About/About'))
+const ExperiencePage = React.lazy(() => import('./components/History/History'))
+const ProjectPage = React.lazy(() => import('./components/Projects/Projects'))
+// const ContactPage = React.lazy(() => import('./components/Home/Home'))
 function Main() {
+    const [loading,setLoading]=useState(false);
     return (
         <BrowserRouter>
             <Navbar></Navbar>
             <React.Suspense fallback={<Loading/>}>
             <Switch>
-            <Route path="/" exact><Home></Home></Route>
-            <Route path="/About" component={About}/>
-            <Route path="/Experience" component={History}/>
-            <Route path="/Portfolio" component={Projects}/>
+            <Route path="/" exact><HomePage/></Route>
+            <Route path="/About" component={AboutPage}/>
+            <Route path="/Experience" component={ExperiencePage}/>
+            <Route path="/Portfolio" component={ProjectPage}/>
       
             </Switch>
             </React.Suspense>

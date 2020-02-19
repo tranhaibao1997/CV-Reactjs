afdf994e9d5c5fbe9bcb19b68adeb36ed36155af
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-02-19 14:20:21 +0700

    add map

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -66,6 +66,9 @@ export default function Contact() {
                         </SlowFadeUp3>
                     </li>
                 </ul>
+                
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3919.4616337330926!2d106.63898441533422!3d10.775911862148549!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752ea3f54477ab%3A0xec28093fe3d91362!2zVHLhu4tuaCDEkMOsbmggVHLhu41uZywgUGjDuiBUcnVuZywgVMOibiBQaMO6LCBI4buTIENow60gTWluaCwgVmlldG5hbQ!5e0!3m2!1sen!2s!4v1582096609894!5m2!1sen!2s" width={'100%'} height={450} frameBorder={0} style={{border: 0}} allowFullScreen />
+
             </section>
             </FadeLeft>
         </>

102bf62f14336bfec925d2342dddddbf5ea980c3
Tran Hai Bao <tranhaibao1997@gmail.com>
2020-02-21 12:20:26 +0700

    fix drive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -68,7 +68,7 @@ export default function About() {
               </Shake>
 
             </ul>
-            <Bounce>  <button className="btn"><a id="CV-Download" href="https://drive.google.com/open?id=1vle9azcMv4i6RSH6Z84dBd59KCST8dq-"><i className="fa fa-download" style={{ marginRight: '10px' }} aria-hidden="true" />Download CV</a> </button></Bounce>
+            <Bounce>  <button className="btn"><a id="CV-Download" href="https://drive.google.com/open?id=1IahsJ-Jg4HDox2QEt2moeFJnQ0kXGI4w"><i className="fa fa-download" style={{ marginRight: '10px' }} aria-hidden="true" />Download CV</a> </button></Bounce>
           </div>
         </div>
       </section>
